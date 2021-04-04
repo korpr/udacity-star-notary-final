816809a24b1df5b70db203570375993375f883b6
rkr <inghamway@gmail.com>
2021-04-04 13:47:50 +0300

    #2 Add token name and Symbol support

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -5,43 +5,52 @@ import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol"
 
 // StarNotary Contract declaration inheritance the ERC721 openzeppelin implementation
 contract StarNotary is ERC721 {
-
     // Star data
     struct Star {
         string name;
     }
 
     // Implement Task 1 Add a name and symbol properties
+
     // name: Is a short name to your token
+    string public _name;
     // symbol: Is a short string like 'USD' -> 'American Dollar'
-    
+    string private _symbol;
+
+    constructor(string memory name, string memory symbol) public {
+        _name = name;
+        _symbol = symbol;
+    }
 
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
     // mapping the TokenId and price
     mapping(uint256 => uint256) public starsForSale;
 
-    
     // Create Star using the Struct
-    function createStar(string memory _name, uint256 _tokenId) public { // Passing the name and tokenId as a parameters
-        Star memory newStar = Star(_name); // Star is an struct so we are creating a new Star
-        tokenIdToStarInfo[_tokenId] = newStar; // Creating in memory the Star -> tokenId mapping
-        _mint(msg.sender, _tokenId); // _mint assign the the star with _tokenId to the sender address (ownership)
+    function createStar(string memory name, uint256 tokenId) public {
+        // Passing the name and tokenId as a parameters
+        Star memory newStar = Star(name); // Star is an struct so we are creating a new Star
+        //TODO:: generae  token id
+        tokenIdToStarInfo[tokenId] = newStar; // Creating in memory the Star -> tokenId mapping
+        _mint(msg.sender, tokenId); // _mint assign the the star with _tokenId to the sender address (ownership)
     }
 
     // Putting an Star for sale (Adding the star tokenid into the mapping starsForSale, first verify that the sender is the owner)
     function putStarUpForSale(uint256 _tokenId, uint256 _price) public {
-        require(ownerOf(_tokenId) == msg.sender, "You can't sale the Star you don't owned");
+        require(
+            ownerOf(_tokenId) == msg.sender,
+            "You can't sale the Star you don't owned"
+        );
         starsForSale[_tokenId] = _price;
     }
-
 
     // Function that allows you to convert an address into a payable address
     function _make_payable(address x) internal pure returns (address payable) {
         return address(uint160(x));
     }
 
-    function buyStar(uint256 _tokenId) public  payable {
+    function buyStar(uint256 _tokenId) public payable {
         require(starsForSale[_tokenId] > 0, "The Star should be up for sale");
         uint256 starCost = starsForSale[_tokenId];
         address ownerAddress = ownerOf(_tokenId);
@@ -49,13 +58,17 @@ contract StarNotary is ERC721 {
         _transferFrom(ownerAddress, msg.sender, _tokenId); // We can't use _addTokenTo or_removeTokenFrom functions, now we have to use _transferFrom
         address payable ownerAddressPayable = _make_payable(ownerAddress); // We need to make this conversion to be able to use transfer() function to transfer ethers
         ownerAddressPayable.transfer(starCost);
-        if(msg.value > starCost) {
+        if (msg.value > starCost) {
             msg.sender.transfer(msg.value - starCost);
         }
     }
 
     // Implement Task 1 lookUptokenIdToStarInfo
-    function lookUptokenIdToStarInfo (uint _tokenId) public view returns (string memory) {
+    function lookUptokenIdToStarInfo(uint256 _tokenId)
+        public
+        view
+        returns (string memory)
+    {
         //1. You should return the Star saved in tokenIdToStarInfo mapping
     }
 
@@ -73,4 +86,19 @@ contract StarNotary is ERC721 {
         //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
     }
 
+    /**
+     * @dev Gets the token name
+     * @return string representing the token name
+     */
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Gets the token symbol
+     * @return string representing the token symbol
+     */
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
 }
